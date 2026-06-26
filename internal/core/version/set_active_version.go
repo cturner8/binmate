4cb61bea6b57c8a973d7aeda5e59f219bec0d627
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-06-26 10:50:47 +0000

    update binary alias to be in addition to main symlink

--- internal/core/version/set_active_version.go
+++ internal/core/version/set_active_version.go
@@ -20,13 +20,7 @@ func SetActiveVersion(versionPath string, initialInstallPath string, binaryName 
 		return "", fmt.Errorf("unable to create install path: %s", err)
 	}
 
-	// Use alias if provided, otherwise use binary name
-	symlinkName := binaryName
-	if alias != nil && *alias != "" {
-		symlinkName = *alias
-	}
-
-	targetInstallPath := path.Join(installPath, symlinkName)
+	targetInstallPath := path.Join(installPath, binaryName)
 
 	// Remove existing symlink/file if present
 	_, err = os.Lstat(targetInstallPath)
@@ -43,6 +37,32 @@ func SetActiveVersion(versionPath string, initialInstallPath string, binaryName 
 	err = os.Symlink(versionPath, targetInstallPath)
 	if err != nil {
 		return "", fmt.Errorf("unable to create symlink: %w", err)
+	}
+
+	// Add additional symlinks for aliases if provided
+	aliases := []*string{}
+	if alias != nil {
+		aliases = append(aliases, alias)
+	}
+
+	for _, alias := range aliases {
+		aliasPath := path.Join(installPath, *alias)
+		// Remove existing symlink/file if present
+		_, err = os.Lstat(aliasPath)
+		if err == nil {
+			// File exists, remove it
+			if err := os.Remove(aliasPath); err != nil {
+				return "", fmt.Errorf("unable to remove existing alias symlink: %w", err)
+			}
+		} else if !errors.Is(err, os.ErrNotExist) {
+			// Error other than "file not found" - could be permission issue, etc.
+			return "", fmt.Errorf("unable to check existing alias symlink: %w", err)
+		}
+
+		err = os.Symlink(versionPath, aliasPath)
+		if err != nil {
+			return "", fmt.Errorf("unable to create alias symlink: %w", err)
+		}
 	}
 
 	return targetInstallPath, nil
